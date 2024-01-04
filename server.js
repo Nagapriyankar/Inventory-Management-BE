fdeb13e8118cb31aae9b567ea9292410e5acfa8a
Nagapriyankar <nagapriyankar@gmail.com>
2024-01-04 00:41:30 -0800

    added middlewares and test the application

--- server.js
+++ server.js
@@ -6,6 +6,23 @@ const cors = require("cors")
 
 //initialize app 
 const app = express()
+
+//middleware
+//helps to handle json data in application 
+app.use(express.json())
+
+//help to handle data that comes via url
+app.use(express.urlencoded({ extended: false }))
+
+//helps to parse the data passing from FE to BE, convert to object for easy access
+app.use(bodyParser.json())
+
+//routes
+app.get("/", (req, res) => { 
+    res.send('Home Page')
+})
+
+
 //initialize port
 const PORT = process.env.port || 5000
 
