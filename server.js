3ec816cb9b48c29af4306e1fa5682046bb0eff32
Nagapriyankar <nagapriyankar@gmail.com>
2024-02-10 03:26:32 -0800

    added cors url

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(cookieParser())  //to create http only cookie as response to front end
 app.use(express.urlencoded({ extended: false })) //help to handle data that comes via url
 app.use(bodyParser.json()) //helps to parse the data passing from FE to BE, convert to object for easy access
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://inventory-management.vercel.app", "https://inventory-management.onerender.com"],
     credentials: true,
 })) //helps to resolve conflicts while makn requst from BE TO FE
 
